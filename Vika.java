fc199ac6204ae0dfc15d866209e19ff834992374
ab3victoria <ab3victoria@gmail.com>
2022-03-13 13:33:15 +0200

    second change - another line added

--- Vika.java
+++ Vika.java
@@ -2,6 +2,8 @@ public class Vika{
 	
 	public static void main(String[] args){
 		
+		System.out.println("*******************");
+		
 		for (int i = 0 ; i <=5 ; i++){
 			System.out.println("My name is Vika");
 		}
